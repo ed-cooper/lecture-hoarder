c1f8820f1b8dc0199098f7b76ab47ce56c798f2b
Edward Cooper <edward.cooper100@gmail.com>
2019-05-11 11:34:34 +0100

    Get login working

---
+++ run.py
@@ -0,0 +1,54 @@
+import requests
+import settings
+import sys
+from bs4 import BeautifulSoup
+
+# First, get login page for hidden params
+getLoginService = requests.get("https://login.manchester.ac.uk/cas/login")
+
+# Check status code valid
+if getLoginService.status_code != 200:
+    print("Could not get login page: service responded with status code", getLoginService.status_code)
+    sys.exit(1)
+
+# Status code valid, parse HTML
+getLoginSoup = BeautifulSoup(getLoginService.content, features="html.parser")
+param_execution = getLoginSoup.find("input", {"name": "execution"})["value"]
+param_lt = getLoginSoup.find("input", {"name": "lt"})["value"]
+
+# Send login request
+postLoginService = requests.post("https://login.manchester.ac.uk/cas/login",
+                                 {"username": settings.username,
+                                  "password": settings.password,
+                                  "lt": param_lt,
+                                  "execution": param_execution,
+                                  "_eventId": "submit",
+                                  "submit": "Login"})
+
+# Check status code valid
+if postLoginService.status_code != 200:
+    print("Could not log in: service responded with status code", postLoginService.status_code)
+    sys.exit(2)
+
+# Status code valid, parse HTML
+postLoginSoup = BeautifulSoup(postLoginService.content, features="html.parser")
+login_result_div = postLoginSoup.find("div", {"id": "msg"})
+
+# Check if login successful
+if "errors" in login_result_div["class"]:
+    print("Login failed:", login_result_div.string)
+    sys.exit(3)
+
+# Login successful
+
+# Get list of courses from video page
+getVideoServiceBase = requests.get("https://video.manchester.ac.uk/")
+
+# Check status code valid
+if getVideoServiceBase.status_code != 200:
+    print("Could not get video service: service responded with status code", getVideoServiceBase.status_code)
+    sys.exit(4)
+
+# Print result
+getVideoServiceBaseSoup = BeautifulSoup(getVideoServiceBase.content, features="html.parser")
+print(getVideoServiceBaseSoup.prettify())
---
+++ settings-template.py
@@ -0,0 +1,3 @@
+username = "Your username"
+password = "Your password"
+base_dir = "~/Documents/Lectures"
