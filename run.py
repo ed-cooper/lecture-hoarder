255ffe327f1d36503e1ee8837a56ae16cc5c7d6d
Chandler Newman <chandler2newman@hotmail.co.uk>
2019-05-11 17:08:35 +0100

    Add concurrent downloading

--- run.py
+++ run.py
@@ -5,6 +5,7 @@ import shutil
 import string
 import sys
 from bs4 import BeautifulSoup
+import concurrent.futures
 
 # The list of characters that can be used in filenames
 VALID_FILE_CHARS = "-_.() %s%s" % (string.ascii_letters, string.digits)
@@ -61,72 +62,90 @@ if get_video_service_base.status_code != 200:
     print("Could not get video service: service responded with status code", get_video_service_base.status_code)
     sys.exit(4)
 
-# Status code valid, parse HTML
-get_video_service_base_soup = BeautifulSoup(get_video_service_base.content, features="html.parser")
-first = True
-for course_li in get_video_service_base_soup.find("nav", {"id": "sidebar-nav"}).ul.contents[3].find_all("li", {
-        "class": "series"}):
-    # For each course
-    print("-" * (21 + len(course_li.a.string)))
-    print("Getting podcasts for", course_li.a.string)
-    print("-" * (21 + len(course_li.a.string)))
-    get_video_service_course = session.get("https://video.manchester.ac.uk" + course_li.a["href"])
+
+def download_podcast(name, podcast_src, download_path):
+    print("Downloading podcast", name)
+
+    # Get podcast
+    get_video_service_podcast = session.get(podcast_src, stream=True)
 
     # Check status code valid
-    if get_video_service_course.status_code != 200:
-        print("Could not get podcasts for", course_li.a.string, "- Service responded with status code",
-              get_video_service_course.status_code)
-        continue
+    if get_video_service_podcast.status_code != 200:
+        print("Could not get podcast for", name, "- Service responded with status code",
+              get_video_service_podcast.status_code)
+        return
 
-    # Success code valid, create directory for podcasts
-    course_dir = os.path.expanduser(os.path.join(settings.base_dir, "".join(
-        c for c in course_li.a.string if c in VALID_FILE_CHARS)))
-    os.makedirs(course_dir, exist_ok=True)
+    # Write to file
+    with open(download_path, "wb") as f:
+        get_video_service_podcast.raw.decode_content = True
+        shutil.copyfileobj(get_video_service_podcast.raw, f)
 
-    # Parse HTML
-    get_video_service_course_soup = BeautifulSoup(get_video_service_course.content, features="html.parser")
-    podcasts = get_video_service_course_soup.find("nav", {"id": "sidebar-nav"}).ul.contents[5].find_all("li", {
-        "class": "episode"})
-    podcast_no = len(podcasts) + 1
-    for podcast_li in podcasts:
-        # For each podcast
-        podcast_no -= 1
+    print("Downloaded podcast", name)
 
-        # Check podcast not already downloaded
-        download_path = os.path.expanduser(os.path.join(course_dir, f"{podcast_no:02d} - " + podcast_li.a.string +
-                                                        ".mp4"))
-        if os.path.isfile(download_path):
-            print("Skipping podcast", podcast_li.a.string, "(already exists)")
-            continue
 
-        # Podcast not yet downloaded
-        print("Getting podcast", podcast_li.a.string)
+with concurrent.futures.ThreadPoolExecutor(max_workers=settings.concurrent_downloads) as executor:
+    futures = []
 
-        # Get podcast webpage
-        get_video_service_podcast_page = session.get("https://video.manchester.ac.uk" + podcast_li.a["href"])
+    # Status code valid, parse HTML
+    get_video_service_base_soup = BeautifulSoup(get_video_service_base.content, features="html.parser")
+    first = True
+    for course_li in get_video_service_base_soup.find("nav", {"id": "sidebar-nav"}).ul.contents[3].find_all("li", {
+        "class": "series"}):
+        # For each course
+        print("-" * (21 + len(course_li.a.string)))
+        print("Getting podcasts for", course_li.a.string)
+        print("-" * (21 + len(course_li.a.string)))
+        get_video_service_course = session.get("https://video.manchester.ac.uk" + course_li.a["href"])
 
         # Check status code valid
-        if get_video_service_podcast_page.status_code != 200:
-            print("Could not get podcast webpage for", podcast_li.a.string, "- Service responded with status code",
-                  get_video_service_podcast_page.status_code)
+        if get_video_service_course.status_code != 200:
+            print("Could not get podcasts for", course_li.a.string, "- Service responded with status code",
+                  get_video_service_course.status_code)
             continue
 
-        # Status code valid, parse HTML
-        getVideoServicePodcastPageSoup = BeautifulSoup(get_video_service_podcast_page.content,
-                                                       features="html.parser")
-        podcast_src = "https://video.manchester.ac.uk" + \
-                      getVideoServicePodcastPageSoup.find("video", id="video").source["src"]
+        # Success code valid, create directory for podcasts
+        course_dir = os.path.expanduser(os.path.join(settings.base_dir, "".join(
+            c for c in course_li.a.string if c in VALID_FILE_CHARS)))
+        os.makedirs(course_dir, exist_ok=True)
 
-        # Get podcast
-        get_video_service_podcast = session.get(podcast_src, stream=True)
+        # Parse HTML
+        get_video_service_course_soup = BeautifulSoup(get_video_service_course.content, features="html.parser")
+        podcasts = get_video_service_course_soup.find("nav", {"id": "sidebar-nav"}).ul.contents[5].find_all("li", {
+            "class": "episode"})
+        podcast_no = len(podcasts) + 1
+        for podcast_li in podcasts:
+            # For each podcast
+            podcast_no -= 1
 
-        # Check status code valid
-        if get_video_service_podcast.status_code != 200:
-            print("Could not get podcast for", podcast_li.a.string, "- Service responded with status code",
-                  get_video_service_podcast_page.status_code)
-            continue
+            # Check podcast not already downloaded
+            download_path = os.path.expanduser(os.path.join(course_dir, f"{podcast_no:02d} - " + podcast_li.a.string +
+                                                            ".mp4"))
+            if os.path.isfile(download_path):
+                print("Skipping podcast", podcast_li.a.string, "(already exists)")
+                continue
 
-        # Write to file
-        with open(download_path, "wb") as f:
-            get_video_service_podcast.raw.decode_content = True
-            shutil.copyfileobj(get_video_service_podcast.raw, f)
+            # Podcast not yet downloaded
+            print("Queuing podcast", podcast_li.a.string)
+
+            # Get podcast webpage
+            get_video_service_podcast_page = session.get("https://video.manchester.ac.uk" + podcast_li.a["href"])
+
+            # Check status code valid
+            if get_video_service_podcast_page.status_code != 200:
+                print("Could not get podcast webpage for", podcast_li.a.string, "- Service responded with status code",
+                      get_video_service_podcast_page.status_code)
+                continue
+
+            # Status code valid, parse HTML
+            getVideoServicePodcastPageSoup = BeautifulSoup(get_video_service_podcast_page.content,
+                                                           features="html.parser")
+            podcast_src = "https://video.manchester.ac.uk" + \
+                          getVideoServicePodcastPageSoup.find("video", id="video").source["src"]
+
+            # Queue podcast for downloading
+            futures.append(executor.submit(download_podcast, podcast_li.a.string, podcast_src, download_path))
+
+    # Wait for all queued podcasts to download
+    for idx, future in enumerate(concurrent.futures.as_completed(futures)):
+        res = future.result()  # This will also raise any exceptions
+
